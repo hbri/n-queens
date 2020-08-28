a809a54d2f740fb718213f56249d55b26bfd4574
Nathaniel Schwartz <nathanielhschwartz@gmail.com>
2020-08-27 20:25:33 -0400

    passing all tests, somehow

--- src/solvers.js
+++ src/solvers.js
@@ -31,13 +31,44 @@
 //   check last row for queen, if preent, increment counter up
 //   return counter value
 
+var Tree = function(row, col, parent = null) {
+  this.queenRow = row;
+  this.queenCol = col;
+  this.parent = parent;
+};
 
-window.findNPiecesSolution = function(n, piece) {
+Tree.prototype.getNumAncestors = function() {
+  var count = 0;
+  var tree = this;
+  while (tree.parent !== null) {
+    count++;
+    tree = tree.parent;
+  }
+  return count;
+};
+
+Tree.prototype.generateChildren = function(numChildren) {
+  var row = this.getNumAncestors();
+  this.children = [];
+  for (var i = 0; i < numChildren; i++) {
+    this.children.push(new Tree(row, i, this));
+  }
+};
+
+Tree.prototype.toggleBoard = function(board) {
+  var me = this;
+  while (me.queenRow !== undefined) {
+    board.togglePiece(me.queenRow, me.queenCol);
+    me = me.parent;
+  }
+};
+
+window.findNPiecesSolution = function(n, pieces) {
 
   var solution = new Board({n: n});
-  if (piece === 'rook') {
+  if (pieces === 'rooks') {
     solution.conflictChecker = solution.hasAnyRooksConflicts;
-  } else if (piece === 'queen') {
+  } else if (pieces === 'queens') {
     solution.conflictChecker = solution.hasAnyQueensConflicts;
   }
   for (var r = 0; r < n; r++) {
@@ -51,31 +82,81 @@ window.findNPiecesSolution = function(n, piece) {
     }
   }
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  console.log('Single solution for ' + n + ' ' + pieces + ':', JSON.stringify(solution));
   return solution.rows();
 };
 
 window.findNRooksSolution = function(n) {
-  return findNPiecesSolution(n, 'rook');
+  return findNPiecesSolution(n, 'rooks');
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  return findNPiecesSolution(n, 'queen');
+  var root = new Tree();
+  var solution;
+  var traverse = function(tree) {
+    var board = new Board({n: n});
+    tree.toggleBoard(board);
+    if (!board.hasAnyQueensConflicts()) {
+      if (tree.getNumAncestors() === n) {
+        solution = board.rows();
+      } else {
+        if (solution === undefined) {
+          tree.generateChildren(n);
+          for (var i = 0; i < tree.children.length; i++) {
+            var child = tree.children[i];
+            traverse(child);
+          }
+        }
+        //   // return solution;
+        // }
+      }
+    }
+  };
+
+  traverse(root);
+  return solution === undefined ? new Board({n: n}).rows() : solution;
+};
+
+window.countNPiecesSolutions = function(n, pieces) {
+  if (pieces === 'rooks') {
+    Board.prototype.conflictChecker = Board.prototype.hasAnyRooksConflicts;
+  } else if (pieces === 'queens') {
+    Board.prototype.conflictChecker = Board.prototype.hasAnyQueensConflicts;
+  }
+
+  var solutionCount = 0;
+  var root = new Tree();
+
+  var traverse = function(tree) {
+    var board = new Board({n: n});
+    tree.toggleBoard(board);
+    if (!board.conflictChecker()) {
+      if (tree.getNumAncestors() === n) {
+        solutionCount++;
+      } else {
+        tree.generateChildren(n);
+        tree.children.forEach(function(child) {
+          traverse(child);
+        });
+      }
+    }
+  };
+
+  traverse(root);
+
+  console.log('Number of solutions for ' + n + ' ' + pieces + ':', solutionCount);
+  return solutionCount;
+
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  return countNPiecesSolutions(n, 'rooks');
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return solutionCount;
+  debugger;
+  return countNPiecesSolutions(n, 'queens');
 };
